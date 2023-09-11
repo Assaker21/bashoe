112d6e6bccc194ac2d4137e47ac4242082cd4f2d
Assaker21 <charbelassaker@live.com>
2023-09-11 18:33:18 +0300

    added server url

--- src/utils/req.js
+++ src/utils/req.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:1234/api/",
+  baseURL: "https://bashoe-api.onrender.com/api/",
   withCredentials: true
 });
 
