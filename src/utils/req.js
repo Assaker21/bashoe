76291da9f7d589ca66c030143becbae12729cc4f
Assaker21 <charbelassaker@live.com>
2023-09-11 17:50:27 +0300

    server url fix

--- src/utils/req.js
+++ src/utils/req.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const newRequest = axios.create({
-  baseURL: process.env.SRC,
+  baseURL: "https://bashoe-api.onrender.com/api",
   withCredentials: true
 });
 
