05e5c313eea01a26f4dd0e34379289c0c63c9c27
Charbel Assaker <charbelassaker@live.com>
2024-02-20 09:21:21 +0200

    fix: cors

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(
       "http://127.0.0.1:5174",
       "https://bashoe.onrender.com",
       "https://bashoe-admin-dashboard.onrender.com",
+      "https://hoophouse.onrender.com/",
     ],
     credentials: true,
   })
