9b636428d94bd2e4000ba8c67efeae243b41a890
Charbel Assaker <charbelassaker@live.com>
2024-02-20 09:27:51 +0200

    fixes

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -8,13 +8,16 @@ const Footer = () => {
         <div className="footer-info">
           <span className="footer-title">Have any questions? Contact us</span>
           <div className="contact-methods">
-            <span>Phone: +961 48 595 656</span>
-            <span>Email: antonioabdo14@gmail.com</span>
+            <span>Email: hoophouse24@outlook.com</span>
           </div>
         </div>
         <div className="footer-foot">
           Created by
-          <a rel="noreferrer" target="_blank" href="https://charbelassaker.onrender.com">
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://charbelassaker.onrender.com"
+          >
             Charbel Assaker
           </a>
         </div>
