912e7b97b3acafd3baf34596e179f81e838c3459
Charbel Assaker <charbelassaker@live.com>
2024-04-01 00:22:43 +0300

    fix: dir name

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, /*"src/uploads/"*/ path.join(__dirname, "..", "/uploads"));
+    cb(null, /*"src/uploads/"*/ path.join(__dirname, "...", "/uploads"));
     //const dirPath = path.join(__dirname, "/uploads");
   },
   filename: (req, file, cb) => {
