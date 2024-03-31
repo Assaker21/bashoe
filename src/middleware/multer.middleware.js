02d4a186b7db03aac84c98138b8376fa37936c1d
Charbel Assaker <charbelassaker@live.com>
2024-04-01 00:18:52 +0300

    fix: dirnam

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,8 +1,10 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "src/uploads/");
+    cb(null, /*"src/uploads/"*/ path.join(__dirname, "..", "/uploads"));
+    //const dirPath = path.join(__dirname, "/uploads");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
--- src/utils/getAllFiles.js
+++ src/utils/getAllFiles.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 
-const dirPath = "src/uploads/";
+//const dirPath = "src/uploads/";
+const dirPath = path.join(__dirname, "..", "/uploads");
 
 function getAllFiles() {
   return fs.readdirSync(dirPath).map((url) => ({ url }));
